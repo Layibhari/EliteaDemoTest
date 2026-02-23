7ad0469488529e6d207b7cd79082595cd79c6cfc
sergeycherkasovv <iamcherkasov.job@gmail.com>
2026-02-23 17:39:00 +0700

    Owners API added caches

--- src/main/java/org/springframework/samples/petclinic/owner/OwnerRepository.java
+++ src/main/java/org/springframework/samples/petclinic/owner/OwnerRepository.java
@@ -19,6 +19,7 @@ import java.util.List;
 import java.util.Optional;
 
 import jakarta.annotation.Nonnull;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -45,6 +46,7 @@ public interface OwnerRepository extends JpaRepository<Owner, Integer> {
 	 * @return a Collection of matching {@link Owner}s (or an empty Collection if none
 	 * found)
 	 */
+	@Cacheable("owners")
 	Page<Owner> findByLastNameStartingWith(String lastName, Pageable pageable);
 
 	/**
@@ -60,6 +62,7 @@ public interface OwnerRepository extends JpaRepository<Owner, Integer> {
 	 * @throws IllegalArgumentException if the id is null (assuming null is not a valid
 	 * input for id)
 	 */
+	@Cacheable("owners")
 	Optional<Owner> findById(@Nonnull Integer id);
 
 }
