5ed0abb905ff796a2b597252713ccffc319100b1
Talha Keles <kelesstalha@gmail.com>
2026-08-12 00:55:22 +0300

    Migrate from SSH to SSM: attach SSM instance profile, remove SSH ingress rule

--- infra/security_group.tf
+++ infra/security_group.tf
@@ -2,13 +2,6 @@ resource "aws_security_group" "petclinic_sg" {
   name        = "petclinic-sg"
   description = "Allow SSH, HTTP, and PetClinic app port"
 
-  ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 
   ingress {
     description = "HTTP via Nginx"
