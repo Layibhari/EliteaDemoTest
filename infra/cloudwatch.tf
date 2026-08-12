c85b479568c65d07635b4c8989c37b5ff5e56c46
Talha Keles <kelesstalha@gmail.com>
2026-08-12 11:41:19 +0300

    Use existing Team4-SSM-CWlogs-role for SSM and CloudWatch access

--- infra/cloudwatch.tf
+++ infra/cloudwatch.tf
@@ -1,30 +1,3 @@
-resource "aws_iam_role" "ec2_cloudwatch_role" {
-  name = "petclinic-ec2-cloudwatch-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "cloudwatch_agent_policy" {
-  role       = aws_iam_role.ec2_cloudwatch_role.name
-  policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
-}
-
-resource "aws_iam_instance_profile" "ec2_cloudwatch_profile" {
-  name = "petclinic-ec2-cloudwatch-profile"
-  role = aws_iam_role.ec2_cloudwatch_role.name
-}
-
 resource "aws_cloudwatch_log_group" "petclinic_logs" {
   name              = "/petclinic/app-logs"
   retention_in_days = 7
